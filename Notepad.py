b26fb6f81fb2824e30617e5868f01dc2b35564e3
petros <m.r.gatoulis07@gmail.com>
2024-06-03 18:59:10 +0300

    Add files via upload

---
+++ Notepad.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+pyautogui.press("winleft")
+time.sleep(1)
+pyautogui.write("notepad")
+time.sleep(1)
+pyautogui.press("enter")
+time.sleep(0.5)
+pyautogui.write("I've felt the hate rise up in me Kneel down and clear the stone of leaves I wander out where you can't seeInside my shell I wait and bleed I've felt the hate rise up in me Kneel down and clear the stone of leaves I wander out where you can't se Inside my shell I wait and bleed Goodbye! I wipe it off the tile, the light is brighter this time Everything is 3-D blasphemyMy eyes are red and gold, the hair is standing straight upThis is not the way I pictured me I can't control my shakes!How the hell did I get here? Something about this, so very wrongI have to laugh out loud, I wish I didn't like this Is it a dream or a memory? I've felt the hate rise up in me Kneel down and clear the stone of leaves I wander out where you can't see Inside my shell I wait and bleed Get outta my head 'cause I don't need this! Why didn't I see this? I'm a victim Manchurian CandidateI have sinned by just Making my mind up and taking your breath away I've felt the hate rise up in me Kneel down and clear the stone of leaves I wander out where you can't see Inside my shell I wait and bleedKneel down and clear the stone of leaves I wander out where you can't see Inside my shell I wait and bleed Goodbye!You haven't learned a thing I haven't changed a thing The flesh was in my bones The pain runs always freeYou haven't learned a thing I haven't changed a thing The flesh was in my bones Kneel down and clear the stone of leaves Inside my shell I wait and bleed Kneel down and clear the stone of leaves Inside my shell I wait and bleed And it waits for you!")
+time.sleep(30)
+pyautogui.hotkey("Alt", "F4")
